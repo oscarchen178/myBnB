7c9b374cc62dd92200e9fd09f00267694b5763d3
Qi123123Li <java.li@mail.utoronto.ca>
2022-08-05 20:44:23 -0400

    update

--- src/mysqlDao.java
+++ src/mysqlDao.java
@@ -56,19 +56,31 @@ public class mysqlDao {
     }
     private void createListingTable() throws SQLException {
         Statement stat = conn.createStatement();
-        String query ="CREATE TABLE listings(oid INT, Type ENUM('full house','apartment','room'), latitude FLOAT(15) DEFAULT 0, longitude FLOAT(15) DEFAULT 0, `Address` VARCHAR(50), `Characteristics` VARCHAR(20), `Calender_Availability` DATE, `Price` FLOAT(10) UNSIGNED, foreign key (oid) references users(uid));";
+        String query ="CREATE TABLE listings(oid INT, " +
+                "Type ENUM('full house','apartment','room'), " +
+                "latitude FLOAT(15) DEFAULT 0, " +
+                "longitude FLOAT(15) DEFAULT 0," +
+                " `Address` VARCHAR(50), " +
+                "`Characteristics` VARCHAR(20), " +
+                "`Calender_Availability` DATE," +
+                " `Price` FLOAT(10) UNSIGNED," +
+                " foreign key (oid) references users(uid));";
                  stat.executeUpdate(query);
         System.out.println("++ created table: listings");
     }
     private void createHostsTable() throws SQLException {
         Statement stat = conn.createStatement();
-        String query ="CREATE TABLE hosts(hid INT, history TEXT, foreign key (hid) references users(uid));";
+        String query ="CREATE TABLE hosts(hid INT," +
+                " history TEXT, " +
+                "foreign key (hid) references users(uid));";
         stat.executeUpdate(query);
         System.out.println("++ created table: hosts");
     }
     private void createRentersTable() throws SQLException {
         Statement stat = conn.createStatement();
-        String query ="CREATE TABLE renters(rid INT,payment_info INT(16), history TEXT, foreign key (rid) references users(uid));";
+        String query ="CREATE TABLE renters(rid INT,payment_info INT(16), " +
+                "history TEXT, " +
+                "foreign key (rid) references users(uid));";
         stat.executeUpdate(query);
         System.out.println("++ created table: renters");
     }
