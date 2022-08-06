7a4512bb1980bdfbb106ce227f3eda0a6eafc807
oscarchen178 <65813281+oscarchen178@users.noreply.github.com>
2022-08-05 20:39:47 -0400

    format

--- src/mysqlDao.java
+++ src/mysqlDao.java
@@ -52,21 +52,40 @@ public class mysqlDao {
         stat.executeUpdate(query);
         System.out.println("++ created table: users");
     }
+
     private void createListingTable() throws SQLException {
         Statement stat = conn.createStatement();
-        String query ="CREATE TABLE listings(oid INT, Type ENUM('full house','apartment','room'), latitude FLOAT(15) DEFAULT 0, longitude FLOAT(15) DEFAULT 0, `Address` VARCHAR(50), `Characteristics` VARCHAR(20), `Calender_Availability` DATE, `Price` FLOAT(10) UNSIGNED, foreign key (oid) references users(uid));";
-                 stat.executeUpdate(query);
+        String query ="CREATE TABLE listings(" +
+                "oid INT, " +
+                "Type ENUM('full house','apartment','room'), " +
+                "latitude FLOAT(15) DEFAULT 0, " +
+                "longitude FLOAT(15) DEFAULT 0, " +
+                "`Address` VARCHAR(50), " +
+                "`Characteristics` VARCHAR(20), " +
+                "`Calender_Availability` DATE, " +
+                "`Price` FLOAT(10) UNSIGNED, " +
+                "foreign key (oid) references users(uid));";
+        stat.executeUpdate(query);
         System.out.println("++ created table: listings");
     }
+
     private void createHostsTable() throws SQLException {
         Statement stat = conn.createStatement();
-        String query ="CREATE TABLE hosts(hid INT, history TEXT, foreign key (hid) references users(uid));";
+        String query ="CREATE TABLE hosts(" +
+                "hid INT, " +
+                "history TEXT, " +
+                "foreign key (hid) references users(uid));";
         stat.executeUpdate(query);
         System.out.println("++ created table: hosts");
     }
+
     private void createRentersTable() throws SQLException {
         Statement stat = conn.createStatement();
-        String query ="CREATE TABLE renters(rid INT,payment_info INT(16), history TEXT, foreign key (rid) references users(uid));";
+        String query ="CREATE TABLE renters(" +
+                "rid INT, " +
+                "payment_info INT(16), " +
+                "history TEXT, " +
+                "foreign key (rid) references users(uid));";
         stat.executeUpdate(query);
         System.out.println("++ created table: renters");
     }
